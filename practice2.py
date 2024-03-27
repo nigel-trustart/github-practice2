1353de10789af159dfe8c41dff606e5fa8710c75
nigel-trustart <163224309+nigel-trustart@users.noreply.github.com>
2024-03-27 12:02:10 +0900

    break code

--- practice2.py
+++ practice2.py
@@ -1,6 +1,6 @@
 import requests
 
-def download_file(url, save_path,max_retries=3):
+def file_download(url, save_path,max_retries=3):
     response = requests.get(url)
     retries=0
     while retries<max_retries:
@@ -16,4 +16,4 @@ def download_file(url, save_path,max_retries=3):
 if __name__ == "__main__":
     url = "http://example.com/samplefile.txt"  # Replace with the URL of the file you want to download
     save_path = "samplefile.txt"  # Replace with the path where you want to save the file
-    download_file(url, save_path)
+    file_download(url, save_path)
