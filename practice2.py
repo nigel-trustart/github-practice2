255acbf4758c95b5b90042b813a5863173202875
nigel-trustart <163224309+nigel-trustart@users.noreply.github.com>
2024-03-27 11:49:15 +0900

    refactoring changes

--- practice2.py
+++ practice2.py
@@ -1,5 +1,4 @@
 import requests
-import os
 
 def download_file(url, save_path,max_retries=3):
     response = requests.get(url)
@@ -10,18 +9,11 @@ def download_file(url, save_path,max_retries=3):
                 f.write(response.content)
             print(f"File downloaded successfully to {save_path}")
         else:
-            print(f"Failed to download file from {url} because of {status_code}")
+            print(f"Failed to download file from {url} because of {response.status_code}")
             print("Retrying download......")
             retries+=1
 
-def main():
+if __name__ == "__main__":
     url = "http://example.com/samplefile.txt"  # Replace with the URL of the file you want to download
     save_path = "samplefile.txt"  # Replace with the path where you want to save the file
-
-    if os.path.exists(save_path):
-        print(f"File {save_path} already exists.")
-    else:
-        download_file(url, save_path)
-
-if __name__ == "__main__":
-    main()
+    download_file(url, save_path)
