161fa8ef6c5067fa68f795805143b5207fdfeb99
nigel-trustart <163224309+nigel-trustart@users.noreply.github.com>
2024-03-27 11:41:58 +0900

    retrial when failed

--- practice2.py
+++ practice2.py
@@ -1,14 +1,18 @@
 import requests
 import os
 
-def download_file(url, save_path):
+def download_file(url, save_path,max_retries=3):
     response = requests.get(url)
-    if response.status_code == 200:
-        with open(save_path, 'wb') as f:
-            f.write(response.content)
-        print(f"File downloaded successfully to {save_path}")
-    else:
-        print(f"Failed to download file from {url}")
+    retries=0
+    while retries<max_retries:
+        if response.status_code == 200:
+            with open(save_path, 'wb') as f:
+                f.write(response.content)
+            print(f"File downloaded successfully to {save_path}")
+        else:
+            print(f"Failed to download file from {url} because of {status_code}")
+            print("Retrying download......")
+            retries+=1
 
 def main():
     url = "http://example.com/samplefile.txt"  # Replace with the URL of the file you want to download
